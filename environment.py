c9afad543bd242f2a079314439f33f48404d28c9
nickolaz <elanir358@gmail.com>
2018-12-03 17:53:41 -0500

    automate base login functionality

--- environment.py
+++ environment.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
+from pages.LoginPage import LoginForm
 
-
-def before_scenario(context, scenario):
+def before_feature(context, scenario):
     # for local driver
     context.driver = webdriver.Chrome()
     context.driver.implicitly_wait(10)
 
+def before_scenario(context, scenario):
+    context.driver.maximize_window()
+    context.driver.get(LoginForm.URL)
+    LoginForm(context.driver).login()
 
-def after_scenario(context, scenario):
+def after_feature(context, scenario):
     context.driver.quit()
--- steps/global_steps.py
+++ steps/global_steps.py
@@ -8,9 +8,10 @@ from pages.LoginPage import LoginForm
 #____________ Global step - login to resource _________
 @Given('Login to bWAPP as a bee user')
 def step_login_as_bee_user(context):
-    context.driver.maximize_window()
-    context.driver.get(LoginForm.URL)
-    LoginForm(context.driver).login()
+    pass
+    #context.driver.maximize_window()
+    #context.driver.get(LoginForm.URL)
+    #LoginForm(context.driver).login()
     #auth_form = LoginForm(context.driver)
     #auth_form.login()
 
