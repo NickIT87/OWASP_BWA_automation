af8f11780b74353e7cc6c27bb962f5b4a7d7fead
nickolaz <nickolaz@localhost.localdomain>
2018-12-05 15:00:15 -0500

    first A1 feature test completed

--- steps/global_steps.py
+++ steps/global_steps.py
@@ -6,18 +6,17 @@ from pages.MainPage import BugListForm
 from pages.elements.A1_HTML_reflectedGET import ActionForm
 
 
-#____________ Global step - select feature to test _________
+#___________ Global 0 step - select feature to test ____________
 @Given('select option {value} in the bug list and press hack')
 def step_select_feature_to_test(context, value):
     BugListForm(context.driver).chose_feature(value)
 
-#________ Step - test HTML injection reflected(GET) ________
+#_____ Step 1 - A1 injection: HTML injection reflected(GET)_____
 @Then('enter data {firstname}, {lastname} and submit the form')
 def step_filling_the_form(context, firstname, lastname):
     ActionForm(context.driver).exploit(firstname, lastname)
 
-#____Step - expected result: injection code not included____
-#_______ "<div id=main>" include <firstname> string ________
+#_____ Step 2 - A1 injection: HTML injection reflected(GET)_____
 @When('the form is submited this {firstname} string must be including into div#main')
 def step_check_expected_result(context, firstname):
     ActionForm(context.driver).check_actual_result(firstname)
