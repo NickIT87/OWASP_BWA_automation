2650d3c002ef3ac4e6c89e2fc4fff88f44c8a6f7
Nickolazz1 <elanir358@gmail.com>
2020-12-23 12:30:20 +0200

    remote web driver

--- environment.py
+++ environment.py
@@ -1,10 +1,23 @@
 from selenium import webdriver
 from pages.LoginPage import LoginForm
 
+
 def before_feature(context, scenario):
+    # remote driver
+    capabilities = {
+        "browserName": "chrome",
+        "version": "77.0",
+        "enableVNC": False,
+        "enableVideo": True
+    }
+    context.driver = webdriver.Remote(
+        command_executor="http://192.168.0.103:4444/wd/hub",
+        desired_capabilities=capabilities
+    )
+
     # for local driver
-    context.driver = webdriver.Chrome()
-    context.driver.implicitly_wait(10)
+    # context.driver = webdriver.Chrome()
+    # context.driver.implicitly_wait(10)
 
 def before_scenario(context, scenario):
     context.driver.maximize_window()
@@ -13,4 +26,5 @@ def before_scenario(context, scenario):
     LoginForm(context.driver).login()
 
 def after_feature(context, scenario):
-    context.driver.close()
+    #context.driver.close()
+    pass
